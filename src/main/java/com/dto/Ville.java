4131bfc8cdb1dc59549c0fb3aea9de083ff6c29d
Tess <lerayt@live.fr>
2023-04-22 23:09:43 +0200

    Fix code smells

--- src/main/java/com/dto/Ville.java
+++ src/main/java/com/dto/Ville.java
@@ -5,10 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 @Table(name="ville_france")
 public class Ville {
